65ee4be16bcac07dbf98b50ec0dae26cdd2bcc44
Air <xkrossiapd163@163.com>
2020-03-27 18:29:41 +0800

    Improve health

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.2]
+
+* Improve health
+
 ## [1.0.1]
 
 * Improve health
--- lib/ai_barcode.dart
+++ lib/ai_barcode.dart
@@ -1,7 +1,10 @@
 library ai_barcode;
 
+///
+/// Import
 import 'package:ai_barcode/src/creator/ai_barcode_platform_creator_interface.dart';
 import 'package:ai_barcode/src/scanner/ai_barcode_platform_scanner_interface.dart';
+
 ///
 /// Import
 import 'package:flutter/material.dart';
